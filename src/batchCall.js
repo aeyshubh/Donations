c203b03f9155cf7a462b737810bccb63f24b4496
Shree Shub <shubhamashishpatel@gmail.com>
2023-01-30 21:34:42 +0530

    Updated code

--- src/batchCall.js
+++ src/batchCall.js
@@ -3,19 +3,19 @@ import { customHttpProvider, daiABI } from "./config";
 import { Framework } from "@superfluid-finance/sdk-core";
 import { Button, Form, FormGroup, FormControl, Spinner } from "react-bootstrap";
 import "./batchCall.css";
-import { ethers,Contract} from "ethers";
-import {abi} from "./config2";
+import { ethers, Contract } from "ethers";
+import { abi } from "./config2";
 //will be used to approve super token contract to spend DAI
 async function daiApprove(approveAmount) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-  
-    const sf = await Framework.create({
-      chainId: 5,
-      provider: provider
-    });
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const sf = await Framework.create({
+    chainId: 5,
+    provider: provider
+  });
   const chainId = await window.ethereum.request({ method: "eth_chainId" });
   const signer = provider.getSigner();
-  
+
 
   const DAI = new ethers.Contract(
     "0x88271d333C72e51516B67f5567c728E702b3eeE8",
@@ -41,15 +41,25 @@ async function daiApprove(approveAmount) {
 
 //where the Superfluid logic takes place
 async function executeBatchCall(upgradeAmt, recipient, flowRate) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-  
-    const sf = await Framework.create({
-      chainId: 5,
-      provider: provider
-    });
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  var accounts;
+  if(window.ethereum){
+    try {
+         accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        console.log(accounts)           
+    } catch (error) {
+        console.log(error)
+    }
+}
+  const sf = await Framework.create({
+    chainId: 5,
+    provider: provider
+  });
   const chainId = await window.ethereum.request({ method: "eth_chainId" });
   const signer = provider.getSigner();
-  
+
   const DAIx = await sf.loadSuperToken(
     "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00"
   );
@@ -61,7 +71,7 @@ async function executeBatchCall(upgradeAmt, recipient, flowRate) {
     });
     //upgrade and create stream at once
     const createFlowOperation = DAIx.createFlow({
-      sender: "0xDCB45e4f6762C3D7C61a00e96Fb94ADb7Cf27721",
+      sender: accounts[0],
       receiver: recipient,
       flowRate: flowRate
     });
@@ -86,7 +96,7 @@ async function executeBatchCall(upgradeAmt, recipient, flowRate) {
         );
         //Add a push Notification here
       });
-      
+
   } catch (error) {
     console.log(
       "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
@@ -95,30 +105,51 @@ async function executeBatchCall(upgradeAmt, recipient, flowRate) {
   }
 }
 
-async function scheduleStream(_RA,_startDate,_Amt,_EndDate){
-  preventDefault()
+async function scheduleStream(_RA, _startDate, _Amt, _EndDate) {
   console.log("Helllo");
+  var accounts;
+  if(window.ethereum){
+    try {
+         accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+       // console.log(accounts)           
+    } catch (error) {
+        console.log(error)
+    }
+}
   const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const chainId = 5;
-    const d1 = new Date(_startDate);
-const d2 = new Date(_EndDate);
-/* console.log("Start Date = "+d1.getTime());
-console.log("End Date = "+d2.getTime()); */
-    const signer = provider.getSigner();
-    const contractAddress ='0x8A6f0FC9Fe2c13565a9db3bBE0953C334B4D4bFF';
-    const contractABI = abi;
-    const peer =new Contract(
-      contractAddress,
-      contractABI,
-      signer
-    );
-    const superToken="0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00";
-    console.log("Writing")
-    const coffeeTxn = await peer.createFlowSchedule(superToken,_RA,_startDate,_startDate,315777553760070,_Amt,_EndDate,"0x","0x");
-    await coffeeTxn.wait();
-    console.log("mined", coffeeTxn.hash);
-  
-          console.log("Written");
+  const chainId = await window.ethereum.request({ method: "eth_chainId" });
+  var d1 = new Date(_startDate);
+  var d2 = new Date(_EndDate);
+  const d11 = parseInt(d1.getTime())/1000;
+  const d22 = parseInt(d2.getTime())/1000;
+  const _Amtt = parseInt(_Amt);
+  /* console.log("Start Date = "+d1.getTime());
+  console.log("End Date = "+d2.getTime()); */
+  const signer = provider.getSigner();
+  const contractAddress = "0x8A6f0FC9Fe2c13565a9db3bBE0953C334B4D4bFF";
+  const contractABI = abi;
+  try{
+  const peer = new Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+  const superToken = "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00";
+/*   console.log(typeof(accounts),typeof(_RA), typeof(d11), typeof(d11),typeof(315777553760070), typeof(_Amtt), typeof(d22),"0x", "0x");
+  console.log((accounts),(_RA), (d11), (d11),(315777553760070), (_Amtt), (d22),"0x", "0x");
+ */
+  console.log("Writing");
+ const coffeeTxn = await peer.createFlowSchedule(superToken, _RA, d11, d11,315777553760070, _Amtt, d22, "0x", "0x");
+  console.log("Writing2");
+
+  await coffeeTxn.wait();
+  console.log("mined", coffeeTxn.hash);
+  }catch{
+    console.log("Error");
+  }
+  console.log("Written");
 }
 
 export const BatchCall = () => {
@@ -130,10 +161,10 @@ export const BatchCall = () => {
   const [flowRate, setFlowRate] = useState("");
   const [flowRateDisplay, setFlowRateDisplay] = useState("");
   const [approveAmount, setApproveAmount] = useState("");
-  const [receiversAddress,setRA] = useState("");
-  const[StartDate,setSD]=useState("");
-  const[Amount,setAmount]=useState("");
-  const[EndDate,setED] =useState("");
+  const [receiversAddress, setRA] = useState("");
+  const [StartDate, setSD] = useState("");
+  const [Amountt, setAmountt] = useState("");
+  const [EndDate, setED] = useState("");
   const [isApproveButtonLoading, setIsApproveButtonLoading] = useState(false);
 
   function calculateFlowRate(amount) {
@@ -169,18 +200,18 @@ export const BatchCall = () => {
     setRecipient(() => ([e.target.name] = e.target.value));
   };
 
-const handelRA =(e)=>{
-  setRA(() =>([e.target.name] = e.target.value));
-}
-const handelSD =(e)=>{
-  setSD(() =>([e.target.name] = e.target.value));
-}
-const handelAmount =(e)=>{
-  setAmount(() =>([e.target.name] = e.target.value));
-}
-const handelED =(e)=>{
-  setED(() =>([e.target.name] = e.target.value));
-}
+  const handelRA = (e) => {
+    setRA(() => ([e.target.name] = e.target.value));
+  }
+  const handelSD = (e) => {
+    setSD(() => ([e.target.name] = e.target.value));
+  }
+  const handelAmount = (e) => {
+    setAmountt(() => ([e.target.name] = e.target.value));
+  }
+  const handelED = (e) => {
+    setED(() => ([e.target.name] = e.target.value));
+  }
   const handleApproveAmountChange = (e) => {
     setApproveAmount(() => ([e.target.name] = e.target.value));
   };
@@ -244,27 +275,29 @@ const handelED =(e)=>{
           </BatchCallButton>
         </Form>
       </div>
-      <div> 
+      <div>
         <br>
         </br>
-        <Form>
-        <h2>Schedule Money stream starts here,A stream will be scheduled at 12 AM GMT timezone</h2>
-        <input type ="text" placeholder="Receiver's Address" name ="receiversAddress" onChange ={handelRA} value={receiversAddress}></input>
-        <input type ="date" placeholder="Start Date" onChange ={handelSD} name ="StartDate" value={StartDate}></input>
-        <input type ="Number" placeholder="Amount" name="Amount" onChange ={handelAmount}value={Amount}></input>
-        <input type ="date" placeholder="End Date" name="EndDate" onChange={handelED} value={EndDate}></input>
-        <button
-            onClick={() => {
-              scheduleStream(receiversAddress, StartDate, Amount,EndDate);
+        <form>
+          <h2>Schedule Money stream starts here,A stream will be scheduled at 12 AM GMT timezone</h2>
+          <input type="text" placeholder="Receiver's Address" name="receiversAddress" onChange={handelRA} value={receiversAddress}></input>
+          <input type="date" placeholder="Start Date" onChange={handelSD} name="StartDate" value={StartDate}></input>
+          <input type="Number" placeholder="Amount" name="Amount" onChange={handelAmount} value={Amountt}></input>
+          <input type="date" placeholder="End Date" name="EndDate" onChange={handelED} value={EndDate}></input>
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              scheduleStream(receiversAddress, StartDate, Amountt, EndDate);
             }}
           >
             Click to schedule a stream
           </button>
-        </Form>
+        </form>
       </div>
-  
-      
+
+
     </div>
- 
+
   );
 };
